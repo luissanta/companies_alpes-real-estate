573a6dcd5d2d36276495ef1253d16653f5eddc6a
Luis Fernando santa <l.santa@uniandes.edu.co>
2024-02-25 17:53:44 -0500

    Add domain in lists

---
+++ app/moduls/lists/domain/entities.py
@@ -0,0 +1,22 @@
+"""Entidades del dominio de vuelos
+
+En este archivo usted encontrará las entidades del dominio de vuelos
+"""
+
+from __future__ import annotations
+from dataclasses import dataclass, field
+import app.moduls.lists.domain.value_objects as ov
+from app.seedwork.domain.entities import Entity, RootAggregation
+
+
+@dataclass
+class Estate(Entity):
+    code: ov.Code = field(default_factory=ov.Code)
+    name: ov.Name = field(default_factory=ov.Name)
+
+
+@dataclass
+class List(RootAggregation):
+    estate_id: int = field(hash=True, default=None)
+    code: ov.Code = field(default_factory=ov.Code)
+    name: ov.Name = field(default_factory=ov.Name)
