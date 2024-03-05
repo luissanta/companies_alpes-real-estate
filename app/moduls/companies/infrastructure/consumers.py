e42fd1ecbd7b89130dd702697705fd76c443ed73
JuliethQP <j.quinchia@uniandes.edu.co>
2024-03-04 21:56:19 -0500

    Consumidores

---
+++ app/moduls/companies/infrastructure/consumers.py
@@ -0,0 +1,51 @@
+import pulsar, _pulsar
+from pulsar.schema import *
+import logging
+import traceback
+from app.moduls.companies.infrastructure.schema.v1.events import EventoReservaCreada
+from app.moduls.companies.infrastructure.schema.v1.commands import ComandoCrearReserva
+from app.seedwork.infrastructure import utils
+
+
+def suscribirse_a_eventos():
+    client = None
+    try:
+        client = pulsar.Client(f'pulsar://{utils.broker_host()}:6650')
+        consumer = client.subscribe('audit-events', consumer_type=_pulsar.ConsumerType.Shared,
+                                    subscription_name='audit-sub-events',
+                                    schema=AvroSchema(EventoReservaCreada))
+
+        while True:
+            mensaje = consumer.receive()
+            print(f'Evento recibido: {mensaje.value().data}')
+
+            consumer.acknowledge(mensaje)
+
+        client.close()
+    except:
+        logging.error('ERROR: Suscribiendose al tópico de eventos!')
+        traceback.print_exc()
+        if client:
+            client.close()
+
+
+def suscribirse_a_comandos():
+    client = None
+    try:
+        client = pulsar.Client(f'pulsar://{utils.broker_host()}:6650')
+        consumer = client.subscribe('audit-commands', consumer_type=_pulsar.ConsumerType.Shared,
+                                    subscription_name='audit-sub-commands',
+                                    schema=AvroSchema(ComandoCrearReserva))
+
+        while True:
+            mensaje = consumer.receive()
+            print(f'Comando recibido: {mensaje.value().data}')
+
+            consumer.acknowledge(mensaje)
+
+        client.close()
+    except:
+        logging.error('ERROR: Suscribiendose al tópico de comandos!')
+        traceback.print_exc()
+        if client:
+            client.close()
